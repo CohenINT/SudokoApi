f3baf49b0a3eca2850ef31628859a2ebfa8194a5
CohenINT <moshe22cohen@gmail.com>
2020-04-21 14:53:56 +0300

    initial commit, added two example methods

---
+++ Controllers/SudokuController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Nancy.Json;
+using SudokuSolver.Models;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SudokuSolver.Controllers
+{
+
+    [ApiController]
+    [Route("[controller]")]
+    public class SudokuController : ControllerBase
+    {
+
+       [HttpGet("/api/GetPuzzle")]
+        public ActionResult GetPuzzle()
+        {
+            return Ok(new { Consumes = "application/json", Values = "moshe" });
+
+        }
+
+        [HttpGet("/api/GetList")]
+        public string GetList()
+        {
+            JavaScriptSerializer ser = new JavaScriptSerializer();
+
+            dynamic list = new Board() { Length=81,Name="board1"};
+            var result = ser.Serialize(list);
+
+            return result;
+        }
+
+
+
+
+
+
+    }
+}
---
+++ Models/Board.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SudokuSolver.Models
+{
+    public class Board
+    {
+
+        public string Name { set; get; }
+        public int Length { set; get; }
+    }
+}
