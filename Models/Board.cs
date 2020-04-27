86396c1b26fc7543fa4ea851bcd2d50e9c90ba8f
CohenINT <moshe22cohen@gmail.com>
2020-04-27 12:33:59 +0300

    started to implement backtrack

--- Models/Board.cs
+++ Models/Board.cs
@@ -13,12 +13,22 @@ namespace SudokuSolver.Models
         public Cell[,] ListCells = new Cell[9, 9];
         Dictionary<string, List<int>> GroupsListDict = new Dictionary<string, List<int>>();
 
+        //Temp array, just to make it eaiaser to do backtrack algorithm in case of failed validations after trying all numbers 1-9.
+        List<Cell> grid = new List<Cell>();//81
+
         public string GeneratedTime { set; get; }
 
         public Board()
         {
 
             GenerateBoard();
+        }
+
+        private int GetRandomNumber()
+        {
+            Random rand = new Random();
+            int number = rand.Next(1, 10);
+            return number;
         }
 
         private int GetRandomNumber(int row, int col)
@@ -31,13 +41,9 @@ namespace SudokuSolver.Models
          //so there is a need to change the previous number to find the right state and keeping sudoku rules.
 
 
-            Random rand = new Random();
-            int number = rand.Next(1, 10);
-            return number;
-            /*
-            int ErrorFlag = -1;
+         
 
-            foreach (var trial in this.ListCells[row,col].Tries)
+            foreach (var trial in this.ListCells[row, col].Tries)
             {
                 if (trial.Value == false)
                 {
@@ -46,45 +52,53 @@ namespace SudokuSolver.Models
                     return trial.Key;
                 }
             }
+            //if it returns -1, then all tries are being used. now need to change previous number.
+            return -1;
 
-            return ErrorFlag;
-            */
+        }
+
+        //Checks if tries all numbers from 1-9, if so it would be need to to change the previous number on the gridlist cells.
+        private bool IsAllTriesUsed(int row, int col)
+        {
+            foreach (var trial in this.ListCells[row, col].Tries)
+            {
+                if (trial.Value == false)
+                {
+                    return false;
+                }
+            }
+
+            return true;
         }
 
 
-        private bool IsExistOnColumn(int randNumber, int row)
+        //Validations
+        private bool IsExistOnColumn(int randNumber, int col)
         {
+            for (int row = 0; row < 9; row++)
+            {
+                if (ListCells[row, col].Value == randNumber)
+                {
+                    return true;
+                }
+            }
 
+            return false;
+
+        }
+        private bool IsExistOnRow(int randNumber, int row)
+        {
             for (int col = 0; col < 9; col++)
             {
-                if (randNumber == ListCells[row, col].Value)
+                if (ListCells[row, col].Value == randNumber)
                 {
                     return true;
                 }
-
             }
+
 
             return false;
         }
-
-
-        private bool IsExistOnRow(int randNumber, int col)
-        {
-            for (int row = 0; row > 9; row++)
-            {
-                if (randNumber == ListCells[row, col].Value)
-                {
-                    return true;
-                }
-
-            }
-
-            return false;
-
-        }
-
-
-
         private bool IsExistOnBox(int randNumber, string group)
         {
 
@@ -98,6 +112,59 @@ namespace SudokuSolver.Models
             return true;
 
         }
+
+
+
+        private int TryGenereateNumber(int row, int col, string group, int colReducer)
+        {
+            bool IsExistonColumn = true;
+            bool IsExistonRow = true;
+            bool IsExistonBox = true;
+            int  RandNumber = 0;
+
+            RandNumber = GetRandomNumber(row, col);
+            if (RandNumber == -1)
+            {
+                //TODO: FIND OTHER WAY TO INCREASE REDUCUER FOR COL, AND ALSO ROW IN CASE OF BACKTRACK.
+                return TryGenereateNumber(row, col - colReducer, group, colReducer++);
+            }
+            //Validations
+            IsExistonColumn = IsExistOnColumn(RandNumber, col);
+            IsExistonRow = IsExistOnRow(RandNumber, row);
+            IsExistonBox = IsExistOnBox(RandNumber, group);
+
+            if (IsAllTriesUsed(row, col) == true && (IsExistonColumn || IsExistonRow || IsExistonBox))
+            {
+                return TryGenereateNumber(row, col - colReducer, group, colReducer++);
+            }
+            else if (IsExistonColumn || IsExistonRow || IsExistonBox)
+            {
+                return TryGenereateNumber(row, col, group, 1);
+            }
+
+            return RandNumber;
+
+
+        }
+
+        private void InitilizedBoardNumbers()
+        {
+            int newNumber = 0;
+            for (int row = 0; row < 9; row++)
+            {
+                for (int col = 0; col < 9; col++)
+                {
+                    newNumber = TryGenereateNumber(row, col, ListCells[row, col].Group,1);
+                    ListCells[row, col].Value = newNumber;
+                    ListCells[row, col].Location = $"{row},{col}";
+                    
+                }
+            }
+
+        }
+
+
+
 
         public void GenerateBoard()
         {
@@ -113,7 +180,6 @@ namespace SudokuSolver.Models
             GroupsListDict.Add("grp_h", new List<int>());
             GroupsListDict.Add("grp_i", new List<int>());
 
-
             for (int i = 0; i < 9; i++)
             {
 
@@ -126,53 +192,53 @@ namespace SudokuSolver.Models
                     if (i < 3 && j < 3)
                     {
                         this.ListCells[i, j].Group = "grp_a";
-                        this.GroupsListDict["grp_a"].Add(this.ListCells[i, j].Value);
+                        // this.GroupsListDict["grp_a"].Add(this.ListCells[i, j].Value);
                     }
 
                     else if (i < 3 && j < 6)
                     {
                         this.ListCells[i, j].Group = "grp_b";
-                        this.GroupsListDict["grp_b"].Add(this.ListCells[i, j].Value);
+                        //this.GroupsListDict["grp_b"].Add(this.ListCells[i, j].Value);
                     }
 
                     else if (i < 3 && j < 9)
                     {
                         this.ListCells[i, j].Group = "grp_c";
-                        this.GroupsListDict["grp_c"].Add(this.ListCells[i, j].Value);
+                        // this.GroupsListDict["grp_c"].Add(this.ListCells[i, j].Value);
                     }
 
                     else if (i < 6 && j < 3)
                     {
                         this.ListCells[i, j].Group = "grp_d";
-                        this.GroupsListDict["grp_d"].Add(this.ListCells[i, j].Value);
+                        //  this.GroupsListDict["grp_d"].Add(this.ListCells[i, j].Value);
                     }
 
 
                     else if (i < 6 && j < 6)
                     {
                         this.ListCells[i, j].Group = "grp_e";
-                        this.GroupsListDict["grp_e"].Add(this.ListCells[i, j].Value);
+                        //   this.GroupsListDict["grp_e"].Add(this.ListCells[i, j].Value);
                     }
 
 
                     else if (i < 6 && j < 9)
                     {
                         this.ListCells[i, j].Group = "grp_f";
-                        this.GroupsListDict["grp_f"].Add(this.ListCells[i, j].Value);
+                        //  this.GroupsListDict["grp_f"].Add(this.ListCells[i, j].Value);
                     }
 
 
                     else if (i < 9 && j < 3)
                     {
                         this.ListCells[i, j].Group = "grp_g";
-                        this.GroupsListDict["grp_g"].Add(this.ListCells[i, j].Value);
+                        //  this.GroupsListDict["grp_g"].Add(this.ListCells[i, j].Value);
                     }
 
 
                     else if (i < 9 && j < 6)
                     {
                         this.ListCells[i, j].Group = "grp_h";
-                        this.GroupsListDict["grp_h"].Add(this.ListCells[i, j].Value);
+                        // this.GroupsListDict["grp_h"].Add(this.ListCells[i, j].Value);
                     }
 
 
@@ -180,101 +246,15 @@ namespace SudokuSolver.Models
                     else if (i < 9 && j < 9)
                     {
                         this.ListCells[i, j].Group = "grp_i";
-                        this.GroupsListDict["grp_i"].Add(this.ListCells[i, j].Value);
+                        //  this.GroupsListDict["grp_i"].Add(this.ListCells[i, j].Value);
                     }
-
-
-
-
 
 
                 }
             }
 
 
-
-            for (int i = 0; i < 9; i++)
-            {
-                for (int j = 0, RecursiveColIndex = 1; j < 9; j++)
-                {
-
-                    int RandNumber = GetRandomNumber(i, j);
-                    this.ListCells[i, j].Tries[RandNumber] = true;
-                    bool IsExistonColumn = IsExistOnColumn(RandNumber, i);
-                    bool IsExistonRow = IsExistOnRow(RandNumber, j);
-                    bool IsExistonBox = IsExistOnBox(RandNumber, this.ListCells[i, j].Group);//TOOD: add implemention of function to check if already exist in the close numbers box. need to use dictionary
-
-                    while (IsExistonColumn || IsExistonRow || IsExistonBox)
-                    {
-
-                        //CHECK IF ALL TRIES FOR THIS CELL ARE MARKED AS TRUE (USED ALREADY)
-                        var AllUsedNumbersList = ListCells[i, j].Tries.Where(x => x.Value == false);
-                        if (AllUsedNumbersList.Count() == 9)
-                            while (RecursiveColIndex <= j && (IsExistonColumn || IsExistonRow || IsExistonBox))
-                            {//all already used, start recursive number changing
-
-                                this.ListCells[i, j].Tries[0] = true;
-                                this.ListCells[i, j].Tries[1] = false;
-                                this.ListCells[i, j].Tries[2] = false;
-                                this.ListCells[i, j].Tries[3] = false;
-                                this.ListCells[i, j].Tries[4] = false;
-                                this.ListCells[i, j].Tries[5] = false;
-                                this.ListCells[i, j].Tries[6] = false;
-                                this.ListCells[i, j].Tries[7] = false;
-                                this.ListCells[i, j].Tries[8] = false;
-                                this.ListCells[i, j].Tries[9] = false;
-
-
-                                this.ListCells[i, j- RecursiveColIndex].Tries[0] = true;
-                                this.ListCells[i, j- RecursiveColIndex].Tries[1] = false;
-                                this.ListCells[i, j- RecursiveColIndex].Tries[2] = false;
-                                this.ListCells[i, j- RecursiveColIndex].Tries[3] = false;
-                                this.ListCells[i, j- RecursiveColIndex].Tries[4] = false;
-                                this.ListCells[i, j- RecursiveColIndex].Tries[5] = false;
-                                this.ListCells[i, j- RecursiveColIndex].Tries[6] = false;
-                                this.ListCells[i, j- RecursiveColIndex].Tries[7] = false;
-                                this.ListCells[i, j- RecursiveColIndex].Tries[8] = false;
-                                this.ListCells[i, j- RecursiveColIndex].Tries[9] = false;
-
-
-                                RandNumber = GetRandomNumber(i, j);
-                                this.ListCells[i, j].Tries[RandNumber] = true;
-                                IsExistonColumn = IsExistOnColumn(RandNumber, i);
-                                IsExistonRow = IsExistOnRow(RandNumber, j);
-                                IsExistonBox = IsExistOnBox(RandNumber, this.ListCells[i, j].Group);
-
-
-                                if(!IsExistonColumn && !IsExistonRow && !IsExistonBox)
-                                this.ListCells[i, j - RecursiveColIndex].Value = RandNumber;
-                                
-                                RecursiveColIndex++;
-
-
-
-                            }
-
-
-                        // if so then reset tries variable all back to false,  change the previous number cell value using random function, and repeat.
-                        //??how to change previous number which would be increased (or decresed) up to 0. (first)??
-                        RandNumber = GetRandomNumber(i, j);
-                        this.ListCells[i, j].Tries[RandNumber] = true;
-                        IsExistonColumn = IsExistOnColumn(RandNumber, i);
-                        IsExistonRow = IsExistOnRow(RandNumber, j);
-                        IsExistonBox = IsExistOnBox(RandNumber, this.ListCells[i, j].Group);
-
-
-
-
-                    }
-
-                    this.ListCells[i, j].Value = RandNumber;
-
-
-
-
-                }
-            }
-
+            InitilizedBoardNumbers();
 
 
         }
